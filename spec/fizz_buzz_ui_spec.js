7847bd4933d758b42b39f02e2661a1f35a8afd03
Sam Joseph <tansaku@gmail.com>
2016-02-05 10:47:29 +0000

    gets something working - still can't do more than one test at a time with loading fixtures

--- spec/fizz_buzz_ui_spec.js
+++ spec/fizz_buzz_ui_spec.js
@@ -1,26 +1,35 @@
 describe('FizzBuzz - index.html', function() {
+
   beforeEach(function() {
     jasmine.getFixtures().fixturesPath = 'base/';
-    loadFixtures('index.html');
-    //document.body.innerHTML = __html__['index.html'];
+    loadFixtures('partial.html');
     $.holdReady(false);
   });
 
-  it("displays Fizz if value is divisible with 3", function() {
-    $('#number').val('3');
-    $('#run').trigger('click');
-    expect($('#display_message').text()).toBe('3 returns: Fizz');
-  });
-
-  xit("displays Buzz if value is divisible with 5", function() {
+  it("displays Buzz if value is divisible with 5", function() {
     $('#number').val('5');
     $('#run').trigger('click');
     expect($('#display_message').text()).toBe('5 returns: Buzz');
-  });
 
-  xit("displays FizzBuzz if value is divisible with 15", function() {
+    $('#number').val('3');
+    $('#run').trigger('click');
+    expect($('#display_message').text()).toBe('3 returns: Fizz');
+
     $('#number').val('15');
     $('#run').trigger('click');
     expect($('#display_message').text()).toBe('15 returns: FizzBuzz');
+  });
+
+  // it("displays Fizz if value is divisible with 3", function() {
+  //   debugger
+
+  // });
+
+  xit("displays FizzBuzz if value is divisible with 15", function() {
+
+  });
+
+  afterEach(function() {
+    // jasmine.cleanUp();
   });
 });
