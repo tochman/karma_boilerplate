e4633873e8c8a29af595b548e711c3158690c9ca
tochman <thomas@nocebo.se>
2016-02-06 10:00:37 +0100

    refactored document#ready call on index.html and partial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
-$(document).ready(function() {
+
+var onReadyEvents = function(){
   $('#run').click(function() {
     var num = parseFloat($('#number').val());
     var game = new FizzBuzz();
     var message = game.fizz_buzz(num);
     $('#display_message').html(num + ' returns: ' + message);
   });
-});
+}
